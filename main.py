a6144e0b23c01e0102d665986bd3b576442f5aff
Rah <arrehman757@gmail.com>
2023-11-14 13:00:34 +0530

    Created a question_bank in main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from question_model import Question
+
+from data import question_data
+
+question_bank = []
+
+for item in question_data:
+    q = item['text']
+    a = item['answer']
+    question_bank.append(Question(q,a))
+
+
